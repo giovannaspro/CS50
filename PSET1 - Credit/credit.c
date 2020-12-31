d32e5c1d20b5ea75b4e0487227563cbb651daeea
Giovanna S. Proença <69317476+giovannaspro@users.noreply.github.com>
2020-12-31 19:42:47 -0300

    Update credit.c

--- PSET1 - Credit/credit.c
+++ PSET1 - Credit/credit.c
@@ -1,3 +1,6 @@
+/*Write a program that prompts the user for a credit card number and then reports (via printf) 
+whether it is a valid American Express, MasterCard, or Visa card number, per the definitions of each’s format herein. */
+
 #include <stdio.h>
 #include <cs50.h>
 #include <math.h>
